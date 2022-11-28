a64c27c8be93504dd8b2d4792b78a495ebac9a6a
Ruetsurue <route99101@gmail.com>
2022-11-28 13:22:12 +0400

    product name and price methods refactored

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,17 +6,29 @@ class ProductPage(BasePage):
     def add_product_to_basket(self):
         self.browser.find_element(*ppl.ADD_PRODUCT_BUTTON).click()
 
-    def should_be_added_to_basket_message(self):
-        self.is_element_present(*ppl.PRODUCT_NAME_IN_MSG)
+    def get_product_name(self):
         product_name_on_page = self.browser.find_element(*ppl.PRODUCT_NAME).text
+        return product_name_on_page
+
+    def get_product_price(self):
+        product_price_on_page = self.browser.find_element(*ppl.PRODUCT_PRICE).text
+        return product_price_on_page
+
+    def should_be_added_to_basket_messages(self):
+        product_name = self.get_product_name()
+        product_price = self.get_product_price()
+        self.should_be_product_name_message(product_name_on_page=product_name)
+        self.should_be_basket_price(product_price_on_page=product_price)
+
+    def should_be_product_name_message(self, product_name_on_page):
+        self.is_element_present(*ppl.PRODUCT_NAME_IN_MSG)
         product_name_in_basket = self.browser.find_element(*ppl.PRODUCT_NAME_IN_MSG).text
         error_msg = f"Product name on page and in basket-add notification do not match" \
                     f"\nExpected: '{product_name_on_page}', got: '{product_name_in_basket}'"
         assert product_name_on_page == product_name_in_basket, error_msg
 
-    def should_be_basket_price(self):
+    def should_be_basket_price(self, product_price_on_page):
         self.is_element_present(*ppl.PRODUCT_PRICE_IN_MSG)
-        product_price_on_page = self.browser.find_element(*ppl.PRODUCT_PRICE).text
         product_price_in_msg = self.browser.find_element(*ppl.PRODUCT_PRICE_IN_MSG).text
         error_msg = f"Product price on page and in basket-add notification do not match" \
                     f"\nExpected: '{product_price_on_page}', got: '{product_price_in_msg}'"
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,16 @@
+import pytest
+
 from pages.product_page import ProductPage
 
-PRODUCT_PAGE_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+PRODUCT_PAGE_URLS = [r"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
+                     r"https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+                     ]
 
 
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser_obj=browser, url=PRODUCT_PAGE_URL)
+@pytest.mark.parametrize('product_url', PRODUCT_PAGE_URLS)
+def test_guest_can_add_product_to_basket(browser, product_url):
+    page = ProductPage(browser_obj=browser, url=product_url)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_get_code()
-    page.should_be_added_to_basket_message()
-    page.should_be_basket_price()
+    page.should_be_added_to_basket_messages()
